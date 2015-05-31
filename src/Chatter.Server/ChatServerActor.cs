3430e322f0cb1422d8843c37979e5e643ccf6228
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-06-01 00:11:49 +0300

    ChatServer: share user sessions across actors

--- src/Chatter.Server/ChatServerActor.cs
+++ src/Chatter.Server/ChatServerActor.cs
@@ -6,33 +6,34 @@ namespace Chatter.Server
 {
     public class ChatServerActor : ReceiveActor
     {
-        // TODO: Move sessions outside actor
-        private readonly Dictionary<string, IActorRef> _userSessions;
+        private static readonly Dictionary<string, IActorRef> UserSessions;
+
+        static ChatServerActor()
+        {
+            UserSessions = new Dictionary<string, IActorRef>();
+        }
 
         public ChatServerActor()
         {
-            _userSessions = new Dictionary<string, IActorRef>();
-
             Receive<ClientMessages.SignIn>(x =>
             {
                 Sender.Tell(new ServerMessages.SignInSuccess());
-                _userSessions.Add(x.Login, Sender);
+                UserSessions.Add(x.Login, Sender);
                 BroadcastToAll(new ServerMessages.NewUserConnected(x.Login));
             });
 
             Receive<ClientMessages.SignOut>(x =>
             {
-                _userSessions.Remove(x.Login);
+                UserSessions.Remove(x.Login);
                 Sender.Tell(new ServerMessages.SignOutSuccess());
             });
 
             Receive<ClientMessages.SendMessage>(x => BroadcastToAll(new ServerMessages.NewMessage(x.Login, x.Message)));
         }
 
-        // TODO: Do not tell a sender (a sender should print a message immediately)
-        private void BroadcastToAll<T>(T message)
+        private static void BroadcastToAll<T>(T message)
         {
-            foreach (var session in _userSessions.Values)
+            foreach (var session in UserSessions.Values)
             {
                 session.Tell(message);
             }
